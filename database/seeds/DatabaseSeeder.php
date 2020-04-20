ed266d9bbe79a2d96adc5ba82b3d5173c61ff638
Ruud Schuurmans <ruud.schuurmans@schuurmans-it.nl>
2020-04-20 11:43:07 +0200

    add mailgun support and seeding of admins

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,16 +1,30 @@
 <?php
 
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
 {
-    /**
-     * Seed the application's database.
-     *
-     * @return void
-     */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+        $superAdminEmail = env('SUPER_ADMIN_EMAIL');
+        $superAdminName = env('SUPER_ADMIN_NAME');
+
+        if (empty($superAdminEmail) || empty($superAdminName)) {
+            return;
+        }
+
+        $this->addAdminIfNotExists($superAdminEmail, $superAdminName);
+    }
+
+    private function addAdminIfNotExists(string $email, string $name) {
+        $user = User::where('email', $email)->first();
+        if (empty($user)) {
+            User::create([
+                'email' => $email,
+                'name' => $name,
+                'is_admin' => true,
+            ]);
+        }
     }
 }
